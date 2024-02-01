3eb68758dfb048c790b1ad783b7cd51c70756983
gthomas2 <brudinie@gmail.com>
2024-02-01 20:26:28 +0000

    Update license to match ltijs license. Update README with install instructions. Add node only docker compose file. Modify makezip.sh to output location of zipfile on completion

--- makezip.sh
+++ makezip.sh
@@ -4,5 +4,9 @@ baseDir=$(basename $scriptDir)
 zipName="${baseDir}.zip"
 echo "Zipping to ${zipName}"
 cd ../
+parentDir=$(pwd)
 zip -r6 $zipName $baseDir -x="*.git*" -x="*.idea*" -x="*node_modules*" -x="*DS_Store*" -x="*makezip.sh*"
 cd -
+echo "----------------------------------------------------------------"
+echo "Zipped to $parentDir/${zipName}"
+echo "----------------------------------------------------------------"
